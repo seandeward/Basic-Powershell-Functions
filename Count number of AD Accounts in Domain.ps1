0a939d91242ef039be0a03a531e4d76816ef142a
seandeward <126360814+seandeward@users.noreply.github.com>
2025-11-18 16:39:15 -0500

    Add files via upload

---
+++ Count number of AD Accounts in Domain.ps1
@@ -0,0 +1,9 @@
+﻿# Get a numerical count of all AD user accounts in the domain (includes enabled and disabled, as well as every kind of user and every OU)
+(Get-AdUser -Filter * | Measure-Object).Count 
+
+# Counting only users in "Users" OU.
+$ouPath = ''
+(Get-AdUser -Filter * -searchbase $ouPath | Measure-Object).Count
+
+# Counting only ACTIVE users in "Users"
+(Get-AdUser -Filter 'enabled -eq "true"' -searchbase $ouPath | Measure-Object).Count
